8bf8ae4ea474355afcb42cf7eb02f068030084c2
Nurhan Haqverdiyev <axverdiyevnurxan@gmail.com>
2025-03-07 21:31:34 +0400

    2
    2

--- FirstProject1/src/Lesson35/BoxGenerics.java
+++ FirstProject1/src/Lesson35/BoxGenerics.java
@@ -4,6 +4,9 @@ public class BoxGenerics<T, Y> {
 	// 	// Для X
 	private T item;
 
+	public BoxGenerics(boolean b, String string) {
+	}
+
 	public void setItem(T item) {
 		this.item = item;
 	}
@@ -15,11 +18,16 @@ public class BoxGenerics<T, Y> {
 	// Для Y..ф
 	private T item2;
 
-	public void setItem2(T item,Y item2) {
+	public void setItem2(Y item2) {
 		this.item = item;
 	}
 
 	public T getItem2() {
 		return item2;
+	}
+
+	public void setItem(String string) {
+		//
+		
 	}
 }
--- FirstProject1/src/Lesson35/Main.java
+++ FirstProject1/src/Lesson35/Main.java
@@ -1,19 +1,29 @@
 package Lesson35;
 
-
 public class Main {
 	public static void main(String[] args) {
-		Box box =new Box();
-		box.setName();
-		
-		BoxGenerics<Integer> boxG= new BoxGenerics<Integer>();
-		boxG.setItem(5);
-		
-		BoxGenerics<Long> boxG2= new BoxGenerics<Long>();
-		boxG.setItem(5);
-		
-		BoxGenerics<Byte> boxG3= new BoxGenerics<Byte>();
-		boxG.setItem(5);
+//		Box box = new Box();
+//		box.setName("Emil");
+//		int a = 10;
+//		Integer b = 10;
+//		b = b.MAX_VALUE;
+
+		BoxGenerics<Boolean, String> boxG = new BoxGenerics<>(true, "Smh");
+		System.out.println("Item 1: " + boxG.getItem());
+		System.out.println("Item 2: " + boxG.getItem2());
+
+		boxG.setItem(false);
+		boxG.setItem2("Updated String");
+		System.out.println("Updated Item 1: " + boxG.getItem());
+		System.out.println("Updated Item 2: " + boxG.getItem2());
+
+		BoxGenerics<Long, Integer> boxG2 = new BoxGenerics<>(100L, 200);
+		System.out.println("BoxG2 Item 1: " + boxG2.getItem());
+		System.out.println("BoxG2 Item 2: " + boxG2.getItem2());
+
+		BoxGenerics<Byte, Double> boxG3 = new BoxGenerics<>((byte) 5, 5.5);
+		System.out.println("BoxG3 Item 1: " + boxG3.getItem());
+		System.out.println("BoxG3 Item 2: " + boxG3.getItem2());
 	}
 }
 //byte
