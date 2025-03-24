e48fd6f265577e92dd0142b89b4dac7488fa5d76
Nurhan Haqverdiyev <axverdiyevnurxan@gmail.com>
2025-03-24 21:52:13 +0400

    Lesson39
    Lesson39

--- FirstProject1/src/Lesson39/Main.java
+++ FirstProject1/src/Lesson39/Main.java
@@ -11,13 +11,11 @@ import org.w3c.dom.ls.LSOutput;
 public class Main {
 
 	public static void main(String[] args) {
-		String[] nameStrings = {"Emil", "Aylin", "Nurhan"};
+		String[] nameStrings = { "Emil", "Aylin", "Nurhan" };
 		for (String string : nameStrings) {
 			System.out.println(string);
 		}
-		
-		
-		
+
 		List<String> nameList = new ArrayList<String>();
 		nameList.add("Emil");
 		nameList.add("Nurhan");
@@ -25,32 +23,30 @@ public class Main {
 		nameList.add("Elvin");
 
 		// 1) Преобразование массива в список(List)
-		String[] nameStrings2 = {"Emil", "Aylin", "Nurhan"};
-		
+		String[] nameStrings2 = { "Emil", "Aylin", "Nurhan" };
+
 		List<String> nameList2 = new ArrayList<>(Arrays.asList(nameStrings2));
 		System.out.println(nameList2);
-		
-		
-		
+
 		// 2) Преобразование списка(List) в массив
 		List<String> nameList3 = new ArrayList<String>();
 		nameList3.add("Elvin");
 		nameList3.add("Nara");
 		nameList3.add("Meleyke");
-		
+
 		String[] nameArray = nameList3.toArray(new String[0]);
-		
+
 		for (String s : nameArray) {
 			System.out.println(nameArray);
 		}
-		
+
 		List<String> names = new ArrayList<String>();
 		names.add("Emil");
 		names.add("Nurhan");
 		names.add("Aylin");
-		
-		names.forEach(n->System.out.println(n));
-		
+
+		names.forEach(n -> System.out.println(n));
+
 		// stream api
 		List<Integer> numbers = new ArrayList<Integer>();
 		numbers.add(3);
@@ -62,15 +58,15 @@ public class Main {
 //		System.out.println(numbers);
 //		List<Integer> evenNumbers = numbers.stream().filter(n->n % 2 == 0).collect(Collectors.toList());
 //		System.out.println(evenNumbers);
-		
+
 		// map - проходит по всему листу
-		List<Integer> doubledNumbers = numbers.stream().map(s->s*2).collect(Collectors.toList());
+		List<Integer> doubledNumbers = numbers.stream().map(s -> s * 2).collect(Collectors.toList());
 		System.out.println(doubledNumbers);
-		
-		
-		List<Integer> newNumbers = numbers.stream().filter(a->a%2==1).map(s->s*3).collect(Collectors.toList());
+
+		List<Integer> newNumbers = numbers.stream().filter(a -> a % 2 == 1).map(s -> s * 3)
+				.collect(Collectors.toList());
 		System.out.println(newNumbers);
-		
+
 	}
 // ( )  -> {}
 }
--- FirstProject1/src/Lesson39Exercise/Main.java
+++ FirstProject1/src/Lesson39Exercise/Main.java
@@ -1,10 +1,24 @@
 package Lesson39Exercise;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<Integer> numbers = new ArrayList<Integer>();
+		numbers.add(3);
+		numbers.add(5);
+		numbers.add(7);
+		numbers.add(2);
+		numbers.add(10);
+		
+		
+		List<Integer> newNumbers = numbers.stream().filter(a -> a % 5 == 1).map(s -> s * 2)
+				.collect(Collectors.toList());
+		System.out.println(newNumbers);
+		
 	}
 
 }
